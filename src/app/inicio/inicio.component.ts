56e63c49b88f1d08a142d169296832beb14cadbf
Nicolas Gamarra <nicogamarra2010@gmail.com>
2022-11-10 13:32:15 -0300

    validacion formulario patente-inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { InterOpService } from '../servicios/inter-op.service';
 import { LoggedService } from '../servicios/logged.service';
@@ -16,37 +16,43 @@ export class InicioComponent implements OnInit {
   titulo: string = 'Playa'
   // propiedades logged service
   $estado;
-
+  patronPatente:any = {
+    patentesViejas : /^[a-zA-Z]{3}[\d]{3}$/,
+    patentesNuevas : /^[a-zA-Z]{2}[0-9]{3}[a-zA-Z]{2}$/,
+    patentesMotosViejas : /^[0-9]{3}[a-zA-Z]{3}$/,
+    patentesMotosNuevas : /^[a-zA-Z]{1}[0-9]{3}[a-zA-Z]{3}$/,
+    
+  }  
   patenteForm:any
   searchText!: string;
-
-  msgBack(op: string) {
-    if(this.patenteForm.value.patente !== ""){
-      
-      let value = {
-        op: op,
-        item: this.patenteForm.value,
-      }
-      //console.log(value);
-      
-      this.newItemEvent.emit(value);      
-    }else{
-      alert("la patente no puede ser vacia")
-    }
-    this.patenteForm.reset();
-  }
-
-
+  msg: any
 
   constructor(
     private loggedService: LoggedService,  private fb: FormBuilder,
   ) { 
     this.$estado = loggedService.logged$;
     this.patenteForm = this.fb.group({
-      patente: ['']        
+      patente: ['',  [Validators.required, Validators.pattern(this.patronPatente.patentesViejas||this.patronPatente.patentesNuevas||this.patronPatente.patentesMotosViejas||this.patronPatente.patentesMotosNuevas)]]        
     });   
   }
-  msg: any
+  
+  msgBack(op: string) {
+    console.log(this.patenteForm.value.patente);
+      let value = {
+        op: op,
+        item: this.patenteForm.value,
+      }
+      //console.log(value);
+      this.newItemEvent.emit(value);      
+    this.patenteForm.reset(); 
+  }
+
+
   ngOnInit(): void {
   }
+
+  get Patente(){
+    return this.patenteForm.get("patente")
+  }
+
 }
