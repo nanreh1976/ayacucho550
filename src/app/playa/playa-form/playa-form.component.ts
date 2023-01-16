e3b8a054e6fafe0ed26e356e2f857b0dd88e3002
crlsh <crlshgimenez@gmail.com>
2023-01-16 15:28:24 -0300

    aviso caja cerrada

--- src/app/playa/playa-form/playa-form.component.ts
+++ src/app/playa/playa-form/playa-form.component.ts
@@ -319,7 +319,6 @@ export class PlayaFormComponent implements OnInit {
       case 'Editar': {
         Swal.fire({
           title: '¿Desea confirmar las modificaciones?',
-          //text: "You won't be able to revert this!",
           icon: 'warning',
           showCancelButton: true,
           confirmButtonColor: '#3085d6',
