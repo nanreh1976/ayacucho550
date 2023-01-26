6a07e903a1f72489cca3737b0a46289c20c1758b
crlsh <crlshgimenez@gmail.com>
2023-01-26 17:59:21 -0300

    boludeces

--- src/app/servicios/autentificacion/auth.service.ts
+++ src/app/servicios/autentificacion/auth.service.ts
@@ -31,7 +31,16 @@ export class AuthService {
   ) {
     /* Saving user data in localstorage when 
     logged in and setting up null when logged out */
-
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user')!);
+      } else {
+        localStorage.setItem('user', 'null');
+        JSON.parse(localStorage.getItem('user')!);
+      }
+    });
   }
 
   // Sign in with email/password
@@ -39,11 +48,12 @@ export class AuthService {
     return this.afAuth
       .signInWithEmailAndPassword(email, password)
       .then((result) => {
-            console.log ("mail user", result.user)
+        this.afAuth.authState.subscribe((user) => {
+          if (user) {
             this.SetUserData(result.user);
             this.router.navigate(['/home']);
-          
-    
+          }
+        });
       })
       .catch((error) => {
         window.alert(error.message);
