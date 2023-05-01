383da452d61a3288a4b5e0da690105da82772f75
crlsh <crlshgimenez@gmail.com>
2023-04-30 22:45:51 -0300

    Dummy Park DB para devel en environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,20 +3,36 @@
 // The list of file replacements can be found in `angular.json`.
 
 
+// Dummypark para devel
 export const environment = {
   firebase: {
-    projectId: 'pfparkapp',
-    appId: '1:1004743501223:web:a0ad3783b0075ea8764282',
-    storageBucket: 'pfparkapp.appspot.com',
-    locationId: 'southamerica-east1',
-    apiKey: 'AIzaSyAU7c6aWYQPt4Z-d6fMkK1m-zhFzhVbwbA',
-    authDomain: 'pfparkapp.firebaseapp.com',
-    messagingSenderId: '1004743501223',
-    measurementId: 'G-E9BWNVQKEG',
+      apiKey: "AIzaSyDyrikGh_AIpTIoIhOaawgGOdk1YrQhSDw",
+      authDomain: "dummy-park.firebaseapp.com",
+      projectId: "dummy-park",
+      storageBucket: "dummy-park.appspot.com",
+      messagingSenderId: "942276751616",
+      appId: "1:942276751616:web:0ea886d0c7fa880f8583e5"
   },
   production: false,
   
 };
+
+// pfpark app database para prod
+
+// export const environment = {
+//   firebase: {
+//     projectId: 'pfparkapp',
+//     appId: '1:1004743501223:web:a0ad3783b0075ea8764282',
+//     storageBucket: 'pfparkapp.appspot.com',
+//     locationId: 'southamerica-east1',
+//     apiKey: 'AIzaSyAU7c6aWYQPt4Z-d6fMkK1m-zhFzhVbwbA',
+//     authDomain: 'pfparkapp.firebaseapp.com',
+//     messagingSenderId: '1004743501223',
+//     measurementId: 'G-E9BWNVQKEG',
+//   },
+//   production: false,
+  
+// };
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
